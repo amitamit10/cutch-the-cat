bffcc5520221e5b7e0226dc64bf95671100148a5
amit <amit.elgabsy@gmail.com>
2025-01-17 13:22:53 +0200

    added the tamplate

---
+++ what i got alrady from nitzanin/constants.py
@@ -0,0 +1,20 @@
+#template
+# screen consts:
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 400
+BG_COLOR = (135, 206, 235)  # Sky blue
+
+# image consts:
+CAT_IMAGE_PATH = "images/cat.png"
+TREE_IMAGE_PATH = "images/tree.png"
+
+CAT_SIZE = (60, 60)
+TREE_SIZE = (200, WINDOW_HEIGHT)
+
+# fonts consts
+FONT_NAME = 'Arial'
+FONT_SIZE = 24
+SCORE_POS = (10, 10)
+TIME_POS = (10, 40)
+GAME_TIME = 30
+
